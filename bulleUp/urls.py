ed18d4e0ffd613afb91efcb7d6b4bbfecf4ce88e
mekaVR <candau.herbert@gmail.com>
2025-04-19 11:59:11 +0200

    add Simple JWT authentication 🪪

--- bulleUp/urls.py
+++ bulleUp/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from authentication.views import *
 from bdtheque.views import *
 from rest_framework.routers import DefaultRouter
@@ -14,6 +16,9 @@ router.register(r'review', ReviewViewSet, basename='review')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', UserRegister.as_view(), name='user-register'),
     path('api/', include(router.urls))
 ]
